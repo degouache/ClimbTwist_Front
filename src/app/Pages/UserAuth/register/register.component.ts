2db9f5fbc319d26e61b9c81b9b0df98ef6e7a3b9
degouache <100487173+degouache@users.noreply.github.com>
2023-01-27 18:21:47 +0100

    Register component
    Register component visual done

--- src/app/Pages/UserAuth/register/register.component.ts
+++ src/app/Pages/UserAuth/register/register.component.ts
@@ -11,5 +11,19 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  onKeyUp(event: any) {
+    const input = event.target;
+    const label = document.querySelector(`label[for=${input.id}]`);
+    if (input.value.length > 0) {
+      if(label){
+        label.classList.add('active');
+      }
+    } else {
+      if(label){
+        label.classList.remove('active');
+      }
+    }
+  }
 
 }
